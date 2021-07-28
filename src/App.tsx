63ab17ae29c2e86e9bc6b3338bf9b1cc3e0655aa
Leticia Cordeiro <72452246+leticiacordeiroo@users.noreply.github.com>
2021-07-28 09:20:50 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-          <Route path="/" exact component={Home}/>
+          <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom}/>
           <Route path="/rooms/:id" component={Room}/>
           <Route path="/admin/rooms/:id" component={AdminRoom} />
